bb28ece7338b1ac1f92f18f14529d1a9487c2b87
张嘉凯 <killicestone@126.com>
2024-02-02 00:32:12 +0800

    init:新增部分文件,修改tabBar

---
+++ my-project/src/pages/list/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component({})
+export default class index extends Vue {
+
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
